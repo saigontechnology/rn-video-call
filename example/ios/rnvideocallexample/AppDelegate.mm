cf33af7cf4f9f2170fb3b17e070cc3705767ec15
Loc NguyenThien <loc.nguyent@saigontechnology.com>
2024-07-26 10:22:39 +0700

    feat: VDC-6 - add firebase configuration

--- example/ios/rnvideocallexample/AppDelegate.mm
+++ example/ios/rnvideocallexample/AppDelegate.mm
@@ -2,6 +2,7 @@
 
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTLinkingManager.h>
+#import <Firebase.h>
 
 @implementation AppDelegate
 
@@ -12,6 +13,8 @@
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
+
+  [FIRApp configure];
 
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
